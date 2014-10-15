d7a130b8fcb43444dbf07efdde3c699e26994564
goldsky <goldsky@fastmail.fm>
2014-10-15 12:07:16 +0700

    clean up old classes

--- core/components/lingua/model/lingua/extresource.class.php
+++
@@ -1,2 +0,0 @@
-<?php
-class extResource extends modDocument {}
--- core/components/lingua/model/lingua/mysql/extresource.class.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once (dirname(dirname(__FILE__)) . '/extresource.class.php');
-class extResource_mysql extends extResource {}
